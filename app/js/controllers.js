fdcdb9da8fec141837dd8a84c5b37aa958da64ca
Joseph Tingsanchali <charnwoot@gmail.com>
2014-08-25 15:26:44 -0500

    first commit, small task manager app with Jacky and Nando

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,72 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('TaskCtrl', ['$scope', function($scope) {
+  	$scope.tasks = [
+  		{
+  			name: "Learn Angular",
+  			date: "8/29/14",
+  			isComplete: false,
+  			dateCompleted: "",
+  			categories: ["program"]
+  		},
+  		{
+  			name: "Learn Polymer",
+  			date: "8/27/14",
+  			isComplete: false,
+  			dateCompleted: "",
+  			categories: ["program","food"]
+  		}
+  	];
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  	$scope.addNewTask = function(newTaskName, newCategory) {
+  		var newCategories = newCategory.replace(" ","").split(","),
+  			newTask = {
+  			name: newTaskName,
+  			date: new Date(),
+  			isComplete: false,
+  			dateCompleted: "",
+  			categories: []
+  		};
+  		
+  		newTask.categories = newCategories;
+
+  		$scope.tasks.push(newTask);
+  	};
+
+
+  	$scope.completeTask = function(task) {
+  		if (task.isComplete === false) {
+  			task.isComplete = true;
+  			task.dateCompleted = new Date();
+  		}
+  	};
+
+  	$scope.showCat = function(cat) {
+  		return function( item ) {
+  			var results;
+  				for (var j = 0; j < item.categories.length; j++){
+  					if (item.categories[j].indexOf(cat) > -1){
+  						return true;
+  					}
+  				};
+  		   return false;
+			};
+		};
 
   }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
